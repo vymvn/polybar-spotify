79a00603f32e64b33c92e291a7013c485056459f
vymvn <vymvn6@gmail.com>
2023-02-28 11:07:45 +0400

    Update hooking method

--- scroll_spotify_status.sh
+++ scroll_spotify_status.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # see man zscroll for documentation of the following parameters
-zscroll -l 30 \
+zscroll -l 25 \
         --delay 0.1 \
-        --scroll-padding "  " \
-        --match-command "`dirname $0`/get_spotify_status.sh --status" \
-        --match-text "Playing" "--scroll 1" \
-        --match-text "Paused" "--scroll 0" \
+        --scroll-padding " " \
+        --match-command "`dirname $0`/get_spotify_status.sh --status 2>/dev/null" \
+        --match-text "" "--scroll 0 --before-text ''" \
+        --match-text "Playing" "--scroll 1 --before-text 'ﱘ '" \
+        --match-text "Paused" "--scroll 0 --before-text 'ﱘ '" \
         --update-check true "`dirname $0`/get_spotify_status.sh" &
 
 wait
